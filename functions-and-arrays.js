9d51d822b2e5b0430550e33e9ee820c936607632
Naga Sai Chaitanya <55658505+naga-sai-chaitanya@users.noreply.github.com>
2020-07-01 11:45:55 +0530

    Add files via upload

---
+++ functions-and-arrays.js
@@ -0,0 +1,119 @@
+// Progression #1: Greatest of the two numbers
+function greatestOfTwoNumbers(a,b){
+  if (a>b){
+    return a
+  }
+  else{
+    return b
+  }
+}
+
+// Progression #2: The lengthy word
+const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
+function findScaryWord(list){
+  let length_list=[];
+  let=c=0;
+  for(c=0;c<list.length;c++){
+    length_list.push(list.length)
+  }
+}
+
+
+// Progression #3: Net Price
+const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+function netPrice(numbers){
+  let sum=0;
+  let i=0;
+  for(i=0;i<numbers.length;i++){
+    sum+=numbers[i];
+  }
+  return sum
+}
+
+
+
+// Progression #4: Calculate the average
+sum=netPrice(numbers)
+n=numbers.length
+console.log(sum/n)
+
+// Progression 4.1: Array of numbers
+const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+
+// Progression 4.2: Array of strings
+const wordsArr = ['seat', 'correspond', 'linen', 'motif', 'hole', 'smell', 'smart', 'chaos', 'fuel', 'palace'];
+function averageWordLength(words){
+  let sum=0;
+  for(let c=0;c<words.length;c++){
+    sum+=words[c];
+  }
+  return sum/words.length
+}
+
+// Progression #5: Unique arrays
+const wordsUnique = [
+  'bread',
+  'jam',
+  'milk',
+  'egg',
+  'flour',
+  'oil',
+  'rice',
+  'coffee powder',
+  'sugar',
+  'salt',
+  'egg',
+  'flour'
+];
+
+// Progression #6: Find elements
+const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
+function searchElement(words,element){
+  let i=0;
+  for(i=0;i<words.length;i++){
+    if(words[i]===element){
+      return true
+    }
+  }
+  return false
+}
+
+// Progression #7: Count repetition
+const wordsCount = [
+  'machine',
+  'matter',
+  'subset',
+  'trouble',
+  'starting',
+  'matter',
+  'eating',
+  'matter',
+  'truth',
+  'disobedience',
+  'matter'
+];
+function howManyTimesElementRepeated(words,word){
+  count=0;
+  for(let a=0;a<words.length;a++){
+    if (words[a]===word){
+      count++;
+    }
+  }
+  return count;
+}
+
+
+// Progression #8: Bonus
+
+const matrix = [
+  [08, 02, 22, 97, 38, 15, 00, 40, 00, 75],
+  [49, 49, 99, 40, 17, 81, 18, 57, 60, 87],
+  [81, 49, 31, 73, 55, 79, 14, 29, 93, 71],
+  [52, 70, 95, 23, 04, 60, 11, 42, 69, 24],
+  [22, 31, 16, 71, 51, 67, 63, 89, 41, 92],
+  [24, 47, 32, 60, 99, 03, 45, 02, 44, 75],
+  [32, 98, 81, 28, 64, 23, 67, 10, 26, 38],
+  [67, 26, 20, 68, 02, 62, 12, 20, 95, 63],
+  [24, 55, 58, 05, 66, 73, 99, 26, 97, 17],
+  [21, 36, 23, 09, 75, 00, 76, 44, 20, 45]
+];
